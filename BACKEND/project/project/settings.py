0a6ad315bf223cec2654029722966fea371490c0
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-07-14 20:14:36 +0530

    completed

--- BACKEND/project/project/settings.py
+++ BACKEND/project/project/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
